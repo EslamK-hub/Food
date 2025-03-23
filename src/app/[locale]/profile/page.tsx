a90c2bb5dc8bfe18c4a27053132d257b8d0f0b38
Eslam Kamal <ek191744@gmail.com>
2025-03-23 23:51:59 +0200

    Added protected routes

---
+++ src/app/[locale]/profile/page.tsx
@@ -0,0 +1,5 @@
+import { Locale } from "@/i18n.config";
+
+export default async function ProfilePage({params} : {params: Promise<{locale: Locale}>}) {
+    return <main></main>;
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { match as matchLocale } from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
-import { i18n, LanguageType } from "./i18n.config";
+import { i18n, LanguageType, Locale } from "./i18n.config";
+import { withAuth } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { Pages, Routes } from "./constants/enums";
 
 function getLocale(request: NextRequest): string | undefined {
     const negotiatorHeaders: Record<string, string> = {};
@@ -23,29 +26,69 @@ function getLocale(request: NextRequest): string | undefined {
     return locale;
 }
 
-export async function middleware(request: NextRequest) {
-    const requestHeaders = new Headers(request.headers);
-    requestHeaders.set("x-url", request.url);
+export default withAuth(
+    async function middleware(request: NextRequest) {
+        const requestHeaders = new Headers(request.headers);
+        requestHeaders.set("x-url", request.url);
 
-    const pathname = request.nextUrl.pathname;
+        const response = NextResponse.next({
+            request: {
+                headers: requestHeaders,
+            },
+        });
 
-    const pathnameIsMissingLocale = i18n.locales.every(
-        (locale) => !pathname.startsWith(`/${locale}`)
-    );
+        const pathname = request.nextUrl.pathname;
 
-    if (pathnameIsMissingLocale) {
-        const locale = getLocale(request);
-        return NextResponse.redirect(
-            new URL(`/${locale}${pathname}`, request.url)
+        const pathnameIsMissingLocale = i18n.locales.every(
+            (locale) => !pathname.startsWith(`/${locale}`)
         );
-    }
 
-    return NextResponse.next({
-        request: {
-            headers: requestHeaders,
+        if (pathnameIsMissingLocale) {
+            const locale = getLocale(request);
+            return NextResponse.redirect(
+                new URL(`/${locale}${pathname}`, request.url)
+            );
+        }
+
+        const currentLocale = request.url.split("/")[3] as Locale;
+        const isAuth = await getToken({ req: request });
+        const isAuthPage = pathname.startsWith(
+            `/${currentLocale}/${Routes.AUTH}`
+        );
+        const protectedRoutes = [Routes.PROFILE, Routes.ADMIN];
+        const isProtectedRoute = protectedRoutes.some((route) =>
+            pathname.startsWith(`/${currentLocale}/${route}`)
+        );
+
+        // If the user is not authenticated and is trying to access a protected route,
+        // redirect them to the login page.
+        if (!isAuth && isProtectedRoute) {
+            return NextResponse.redirect(
+                new URL(
+                    `/${currentLocale}/${Routes.AUTH}/${Pages.LOGIN}`,
+                    request.url
+                )
+            );
+        }
+
+        // If the user is authenticated and is trying to access the auth page,
+        // redirect them to the profile page.
+        if (isAuthPage && isAuth) {
+            return NextResponse.redirect(
+                new URL(`/${currentLocale}/${Routes.PROFILE}`, request.url)
+            );
+        }
+
+        return response;
+    },
+    {
+        callbacks: {
+            authorized() {
+                return true;
+            },
         },
-    });
-}
+    }
+);
 
 export const config = {
     matcher: [
