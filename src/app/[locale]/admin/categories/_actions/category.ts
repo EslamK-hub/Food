4d2516852708593464691053151b072b4647b266
Eslam Kamal <ek191744@gmail.com>
2025-04-12 13:04:18 +0200

    Added edit category modal

--- src/app/[locale]/admin/categories/_actions/category.ts
+++ src/app/[locale]/admin/categories/_actions/category.ts
@@ -4,7 +4,7 @@ import { Pages, Routes } from "@/constants/enums";
 import { getCurrentLocale } from "@/lib/getCurrentLocale";
 import { db } from "@/lib/prisma";
 import getTrans from "@/lib/translation";
-import { addCategorySchema } from "@/validations/category";
+import { addCategorySchema, updateCategorySchema } from "@/validations/category";
 import { revalidatePath } from "next/cache";
 
 export const addCategory = async (prevState: unknown, formData: FormData) => {
@@ -31,6 +31,49 @@ export const addCategory = async (prevState: unknown, formData: FormData) => {
         return {
             status: 201,
             message: translations.messages.categoryAdded,
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            status: 500,
+            message: translations.messages.unexpectedError,
+        };
+    }
+};
+
+export const updateCategory = async (
+    id: string,
+    prevState: unknown,
+    formData: FormData
+) => {
+    const locale = await getCurrentLocale();
+    const translations = await getTrans(locale);
+    const result = updateCategorySchema(translations).safeParse(
+        Object.fromEntries(formData.entries())
+    );
+    if (result.success === false) {
+        return {
+            error: result.error.formErrors.fieldErrors,
+            status: 400,
+        };
+    }
+    const data = result.data;
+
+    try {
+        await db.category.update({
+            where: {
+                id,
+            },
+            data: {
+                name: data.categoryName,
+            },
+        });
+        revalidatePath(`/${locale}/${Routes.ADMIN}/${Pages.CATEGORIES}`);
+        revalidatePath(`/${locale}/${Routes.MENU}`);
+
+        return {
+            status: 200,
+            message: translations.messages.updateCategorySuccess,
         };
     } catch (error) {
         console.error(error);
--- src/app/[locale]/admin/categories/_components/CategoryItem.tsx
+++ src/app/[locale]/admin/categories/_components/CategoryItem.tsx
@@ -1,14 +1,19 @@
 import { Category } from "@prisma/client";
+import EditCategory from "./EditCategory";
+import getTrans from "@/lib/translation";
+import { getCurrentLocale } from "@/lib/getCurrentLocale";
 
 async function CategoryItem({ category }: { category: Category }) {
+    const locale = await getCurrentLocale();
+    const translations = await getTrans(locale);
     return (
         <li className="bg-gray-300 p-4 rounded-md flex justify-between">
             <h3 className="text-black font-medium text-lg flex-1">
                 {category.name}
             </h3>
             <div>
-                <span>edit</span>
-                <span>delete</span>
+                <EditCategory translations={translations} category={category}></EditCategory>
+                {/* <DeleteCategory></DeleteCategory> */}
             </div>
         </li>
     );
